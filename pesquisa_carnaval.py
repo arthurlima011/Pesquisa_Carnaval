cc8c2cb7d223de8851fe4b219ceee25e6cd65fad
Arthur <124075351+Hirato001@users.noreply.github.com>
2023-06-13 15:32:57 -0300

    Add files via upload

---
+++ pesquisa_carnaval.py
@@ -0,0 +1,11 @@
+def perguntas():
+
+    print('Gosta de carnaval?: ')
+    pergunta_carnaval = input()
+
+    print('Gosta de açaí?: ')
+    pergunta_acai = input()
+    
+    print('Consome álcool?: ')
+    pergunta_alcool = input()
+
