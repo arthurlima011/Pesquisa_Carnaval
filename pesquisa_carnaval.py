e37d0be5720d47a94505e76ec71980378ea132c9
Arthur <124075351+Hirato001@users.noreply.github.com>
2023-06-13 16:34:15 -0300

    Add files via upload

--- pesquisa_carnaval.py
+++ pesquisa_carnaval.py
@@ -1,11 +1,27 @@
-def perguntas():
+import mysql.connector
 
-    print('Gosta de carnaval?: ')
-    pergunta_carnaval = input()
+mydb = mysql.connector.connect(
+  host="10.1.1.109",
+  user="admin",
+  password="admin",
+  database=""
+)
 
-    print('Gosta de açaí?: ')
-    pergunta_acai = input()
-    
-    print('Consome álcool?: ')
-    pergunta_alcool = input()
+mycursor = mydb.cursor()
 
+mycursor.execute("create database pesquisa")
+
+mycursor.execute("show databases")
+
+for x in mycursor:
+    print(x)
+#MENU
+def menu():
+    print('''Menu:
+    Olá, as respostas para essas perguntas são anônimas e nos ajudam a melhorar nosso atendimento ao cliente.
+        [1] - Iniciar as perguntas
+        [2] - Sair  ''')
+    int(input('Escolha uma opção: '))
+
+iniciarPerguntas = 1
+sair = 2
